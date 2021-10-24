bb97d78c0de3fafc93ae802b042a7515d7a70463
ma-lalonde <malalonde@sbquantum.com>
2021-10-24 17:00:15 -0400

    Fix genclient

--- docker-compose/openvpn/gen_client_conf.sh
+++ docker-compose/openvpn/gen_client_conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -p "Enter a file name for the new client cert (without the .ovpn extension): <filename>.ovpn " FILENAME
+read -p "Enter a file name for the new client cert (without the .ovpn extension: <filename>.ovpn): " FILENAME
     if [ -n $FILENAME ]
 	then
 		docker-compose down
@@ -8,5 +8,5 @@ read -p "Enter a file name for the new client cert (without the .ovpn extension)
 		docker run -v ovpn-data:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient $FILENAME > $FILENAME.ovpn
 		docker-compose up -d
 	else
-		echo "Please enter a valid file name
+		echo "Please enter a valid file name"
 	fi
