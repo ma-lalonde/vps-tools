cf0290c5c12ce4a86cac4852f58f448d11750001
ma-lalonde <malalonde@sbquantum.com>
2021-10-24 16:07:08 -0400

    Fix ovpn 4096 bits keys

--- docker-compose/openvpn/gen_client_conf.sh
+++ docker-compose/openvpn/gen_client_conf.sh
@@ -4,7 +4,7 @@ read -p "Enter a file name for the new client cert (without the .ovpn extension)
     if [ -n $FILENAME ]
 	then
 		docker-compose down
-		docker-compose run --rm openvpn_udp easyrsa build-client-full $FILENAME
+		docker run -e EASYRSA_KEY_SIZE=4096 -v ovpn-data:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $FILENAME
 		docker-compose run --rm openvpn_udp ovpn_getclient $FILENAME > $FILENAME.ovpn
 		docker-compose up -d
 	else
