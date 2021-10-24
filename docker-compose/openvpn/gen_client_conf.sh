0038a9f10702dc27b31d40c0771976d2a4df0e53
ma-lalonde <malalonde@sbquantum.com>
2021-10-24 16:45:55 -0400

    remove 4096 bits, too slow

--- docker-compose/openvpn/gen_client_conf.sh
+++ docker-compose/openvpn/gen_client_conf.sh
@@ -4,8 +4,8 @@ read -p "Enter a file name for the new client cert (without the .ovpn extension)
     if [ -n $FILENAME ]
 	then
 		docker-compose down
-		docker run -e EASYRSA_KEY_SIZE=4096 -v ovpn-data:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $FILENAME
-		docker-compose run --rm openvpn_udp ovpn_getclient $FILENAME > $FILENAME.ovpn
+		docker run -v ovpn-data:/etc/openvpn --rm -it kylemanna/openvpn easyrsa build-client-full $FILENAME
+		docker run -v ovpn-data:/etc/openvpn --rm kylemanna/openvpn ovpn_getclient $FILENAME > $FILENAME.ovpn
 		docker-compose up -d
 	else
 		echo "Please enter a valid file name
--- docker-compose/openvpn/install.sh
+++ docker-compose/openvpn/install.sh
@@ -4,7 +4,7 @@ source .env
 WAN_IP=$(wget -qO- ifconfig.me/ip)
 docker volume create --name ovpn-data
 docker run -v ovpn-data:/etc/openvpn --rm kylemanna/openvpn ovpn_genconfig -u udp://${SITE} -C 'AES-256-GCM' -a 'SHA384' -p 'route ${WAN_IP} 255.255.255.255 net_gateway' -e 'duplicate-cn' -e 'max-clients 10'
-docker run -e EASYRSA_KEY_SIZE=4096 -v ovpn-data:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
+docker run -v ovpn-data:/etc/openvpn --rm -it kylemanna/openvpn ovpn_initpki
 
 
 sudo sysctl -w net.ipv4.ip_forward=1
