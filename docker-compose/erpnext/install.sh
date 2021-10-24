43c22f13d806278e0875e38c17b5612e8177042e
ma-lalonde <malalonde@sbquantum.com>
2021-10-24 15:28:02 -0400

    replace frappe docker docker-compose.yml with custom one

--- docker-compose/erpnext/install.sh
+++ docker-compose/erpnext/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/frappe/frappe_docker.git
-cp docker-compose.override.yml frappe_docker/docker-compose.override.yml
+cp -f docker-compose.yml frappe_docker/docker-compose.yml
 cp .env frappe_docker/.env
 cd frappe_docker
-docker-compose --project-name erpnext up -d
+docker-compose up -d
