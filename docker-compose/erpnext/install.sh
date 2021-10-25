49be764950ff6d60058a65ef674370b1a123dad5
ma-lalonde <malalonde@sbquantum.com>
2021-10-24 22:23:39 -0400

    ERPNext production version

--- docker-compose/erpnext/install.sh
+++ docker-compose/erpnext/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git clone https://github.com/frappe/frappe_docker.git
-cp -f docker-compose.yml frappe_docker/docker-compose.yml
+cp -f docker-compose-erpnext.yml frappe_docker/installation/docker-compose-erpnext.yml
+cp -f docker-compose-networks.yml frappe_docker/installation/docker-compose-networks.yml
 cp -f .env frappe_docker/.env
 cd frappe_docker
 docker-compose up -d
