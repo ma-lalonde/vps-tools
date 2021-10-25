948ad61bceffad4378ee3248471c0f9924b721ce
ma-lalonde <malalonde@sbquantum.com>
2021-10-24 22:26:50 -0400

    fix

--- docker-compose/erpnext/install.sh
+++ docker-compose/erpnext/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git clone https://github.com/frappe/frappe_docker.git
-cp -f docker-compose-erpnext.yml frappe_docker/installation/docker-compose-erpnext.yml
-cp -f docker-compose-networks.yml frappe_docker/installation/docker-compose-networks.yml
+cp -f docker-compose-erpnext.override.yml frappe_docker/installation/docker-compose-erpnext.yml
+cp -f docker-compose-networks.override.yml frappe_docker/installation/docker-compose-networks.yml
 cp -f .env frappe_docker/.env
 cd frappe_docker
-docker-compose up -d
+docker-compose -f installation/docker-compose-common.yml -f installation/docker-compose-erpnext.yml -f installation/docker-compose-networks.yml up -d
