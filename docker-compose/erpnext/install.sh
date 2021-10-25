48af44403bc68163c07eeb7c111b0d03391a3cea
ma-lalonde <malalonde@sbquantum.com>
2021-10-24 22:42:52 -0400

    revert to single docker compose,.. production build

--- docker-compose/erpnext/install.sh
+++ docker-compose/erpnext/install.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 git clone https://github.com/frappe/frappe_docker.git
-cp -f docker-compose-erpnext.override.yml frappe_docker/installation/docker-compose-erpnext.override.yml
-cp -f docker-compose-networks.override.yml frappe_docker/installation/docker-compose-networks.override.yml
-cp -f .env frappe_docker/installation/.env
+cp -f docker-compose.yml frappe_docker/docker-compose.yml
+cp -f .env frappe_docker/.env
 cd frappe_docker
-docker-compose -f installation/docker-compose-common.yml -f installation/docker-compose-erpnext.yml -f installation/docker-compose-networks.yml up -d
+docker-compose up -d
