f9a17853990ca744f80430d04d1edf7045d0a428
ma-lalonde <malalonde@sbquantum.com>
2021-10-24 23:33:55 -0400

    Update install.sh

--- docker-compose/nginx-reverse-proxy/install.sh
+++ docker-compose/nginx-reverse-proxy/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker network create network-nginx-proxy
+docker network create --attachable=true --driver=bridge --subnet=172.28.0.0/16 --gateway=172.28.0.1 --opt com.docker.network.bridge.name=docker_proxy0 network-nginx-proxy
 docker-compose up -d
